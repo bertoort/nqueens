0c2850814316fcc40012424c17f5d146adeba8bc
berto <berto.ort@gmail.com>
2019-12-17 16:03:13 -0700

    feat: convert to matrix func

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ import "fmt"
 const queen = '♕'
 const box = '☐'
 
+var simpleSolution = []int{0, 4, 7, 5, 2, 6, 1, 3}
 var demoQueens = [][]int{{0, 1}, {1, 3}, {2, 0}, {3, 2}}
 
 func main() {
-	board := generateBoard(demoQueens)
-	fmt.Println(board)
+	eightBoard := generateBoard(convertToMatrix(simpleSolution))
+	fourBoard := generateBoard(demoQueens)
+
+	fmt.Println(eightBoard)
+	fmt.Println(fourBoard)
+}
+
+func convertToMatrix(solution []int) (matrix [][]int) {
+	for x, y := range solution {
+		matrix = append(matrix, []int{x, y})
+	}
+	return
 }
 
 func generateBoard(queens [][]int) (board string) {
